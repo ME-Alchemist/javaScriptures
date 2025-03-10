c8f35a47c9fcb11ddd7170e94e0dbaba162cd8a6
Pedram <pedram.hejazi.kenari@iths.se>
2025-03-10 20:49:20 +0100

    my second commit

--- the-javascriptures/src/components/header.jsx
+++ the-javascriptures/src/components/header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import { Button, Offcanvas } from "react-bootstrap";
+import { useState } from "react";
 
 const StyledHeader = styled.header`
   background: red;
@@ -10,10 +12,30 @@ const StyledHeader = styled.header`
   }
 `;
 const Header = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <StyledHeader>
-      <h1>Welcome traveler!</h1>
-    </StyledHeader>
+    <>
+      <StyledHeader className="shadow-sm d-flex flex-row justify-content-center gap-5">
+        <h1>Welcome traveler!</h1>
+        <Button onClick={handleShow}>Primary</Button>
+      </StyledHeader>
+
+      {show && (
+        <Offcanvas show={show} onHide={handleClose} placement="top">
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            Some text as placeholder. In real life you can have the elements you
+            have chosen. Like, text, images, lists, etc.
+          </Offcanvas.Body>
+        </Offcanvas>
+      )}
+    </>
   );
 };
 
