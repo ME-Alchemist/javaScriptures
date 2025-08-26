3097698bdd20ff3cd19c4e85d29432122195bb99
PHK <pedram.hejazi.kenari@iths.se>
2025-08-26 14:37:35 +0200

    attempting safari solution #2

--- BE/index.js
+++ BE/index.js
@@ -20,6 +20,17 @@ app.use(
       "https://www.javascriptures-1.onrender.com",
     ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "Accept",
+      "Origin",
+    ],
+    exposedHeaders: ["Content-Type", "Authorization"],
+    maxAge: 3600,
+    optionsSuccessStatus: 200,
   })
 );
 app.use(express.json());
